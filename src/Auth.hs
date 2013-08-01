20b735e6211ccbb0b4ce70b152d8b9d0a8b43799
Alex Sayers <alex.sayers@gmail.com>
2013-08-01 14:06:47 +0100

    Document Auth module

--- src/Auth.hs
+++ src/Auth.hs
@@ -22,21 +22,29 @@ import Data.Text.Lazy.Encoding (decodeUtf8)
 import Web.ClientSession (Key, encryptIO, decrypt)
 import Control.Error (hush)
 
+-- | Identity assertion recieved from a user
 data Assertion = Assertion String deriving (Show)
 instance FromJSON Assertion where
   parseJSON (Object v) = Assertion <$> v .: "assertion"
   parseJSON _          = error "Invalid JSON"
 
-data VerifierResponse = VerifierResponse String String deriving (Show)
+-- | The validity of an assertion, as determined by the Persona verifer
+data VerifierResponse = VerifierResponse { verifierStatus :: String
+                                         , verifierEmail :: String
+                                         } deriving (Show)
 instance FromJSON VerifierResponse where
   parseJSON (Object v) = VerifierResponse <$> v .: "status" <*> v .: "email"
   parseJSON _          = error "Invalid JSON"
 
-data AuthToken = AuthToken String String deriving (Show)
+-- | The authentication token of a user already logged in
+data AuthToken = AuthToken { tokenEmail :: String
+                           , tokenSession :: String
+                           } deriving (Show)
 instance FromJSON AuthToken where
   parseJSON (Object v) = AuthToken <$> v .: "email" <*> v .: "session"
   parseJSON _          = error "Invalid JSON"
 
+-- | check the validity of an assertion using the Persona verfier. Thread-blocking.
 checkAssertion :: ByteString -> Assertion -> IO VerifierResponse
 checkAssertion hostUrl (Assertion a) = do
   url <- parseUrl "https://verifier.login.persona.org/verify"
@@ -45,6 +53,7 @@ checkAssertion hostUrl (Assertion a) = do
   -- TODO: check certificate - at present we're open to a MitM
   return . fromJust . decode . responseBody $ response
 
+-- | Check that an auth token was issued by the server, and that the user is who he thinks he is.
 checkAuthToken :: Key -> AuthToken -> Maybe String
 checkAuthToken key (AuthToken email session) =
   case decrypt key $ pack session of
@@ -57,11 +66,13 @@ encryptAndSerialise key msg = do
   encMsg <- encryptIO key . pack $ msg
   return $ decodeUtf8 $ fromChunks [encMsg]
 
+-- | Parse the text of a "Cookie" header, extracting an auth token if the appropriate fields are set
 parseAuthToken :: Text -> Maybe AuthToken
 parseAuthToken str = case parseCookie $ unpack str of
                        Nothing -> Nothing
                        Just c  -> AuthToken <$> lookup "email" c <*> lookup "session" c
 
+-- | Parse the text of a "Cookie" header.
 parseCookie :: String -> Maybe [(String, String)]
 parseCookie = hush . parse cookieFields "(Unknown)"
 
